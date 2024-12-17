b14e2a61d9918de0b759f71d4d6dc3a80e2f1b7e
Yui <yui@osufx.com>
2024-12-17 16:57:00 -0300

    feat: made User type serializable

--- MSBIHelper/Types/User.cs
+++ MSBIHelper/Types/User.cs
@@ -1,20 +1,28 @@
-namespace MSBIHelper.Types;
+using Newtonsoft.Json;
 
+namespace MSBIHelper.Types;
+[JsonObject(MemberSerialization.OptIn)]
 public struct User
 {
+    [JsonProperty]
     public string Name { get; }
+    [JsonProperty]
     public string YoutubeId { get; }
+    [JsonProperty]
     public bool IsSubscriber { get; }
+    [JsonProperty]
     public bool IsModerator { get; }
+    [JsonProperty]
     public bool IsMember { get; }
+    [JsonProperty]
     public bool IsBroadcaster { get; }
-
-    public User(string name, string youtubeId, bool isSub, bool isMod, bool isMember, bool isBroadcaster)
+    [JsonConstructor]
+    public User(string name, string youtubeId, bool isSubscriber, bool isModerator, bool isMember, bool isBroadcaster)
     {
         Name = name;
         YoutubeId = youtubeId;
-        IsSubscriber = isSub;
-        IsModerator = isMod;
+        IsSubscriber = isSubscriber;
+        IsModerator = isModerator;
         IsMember = isMember;
         IsBroadcaster = isBroadcaster;
     }
